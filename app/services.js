6a50d2a956129aea8510e267cd063167df0657ad
asobolev <sobolev.andrey@gmail.com>
2015-09-28 18:38:00 +0200

    prefix parsing workaround

--- app/services.js
+++ app/services.js
@@ -5,8 +5,22 @@ angular.module('services', [])
     var store = rdfstore.create(function(err, store) {});
 
     var update = function (rdfData) {
-        store.load("text/turtle", rdfData, function(err, results){
-            $rootScope.$broadcast('store.update', store);
+
+        // FIXME rdfstore-js does not support PREFIX parsing, workaround here
+
+        var parser = N3.Parser();
+        parser.parse(rdfData, function (err, triple, prefixes) {
+            if (!triple) {
+                for (var prx in prefixes) {
+                    if (Object.keys(store.rdf.prefixes).indexOf(prx) < 0) {
+                        store.setPrefix(prx, prefixes[prx]);
+                    }
+                }
+
+                store.load("text/turtle", rdfData, function(err, results){
+                    $rootScope.$broadcast('store.update', store);
+                });
+            }
         });
     };
 
