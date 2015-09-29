4a303b7237d5ddc4289e087b77ec37bb5400fa1a
asobolev <sobolev.andrey@gmail.com>
2015-09-29 14:31:56 +0200

    resolving prefixes for RDFType list

--- app/controllers.js
+++ app/controllers.js
@@ -2,37 +2,60 @@ angular.module('controllers', ['services'])
 
 .controller('TypesList', ['$scope', 'store', function($scope, store) {
 
-    function exist(lst, prefix, name) {
+    function RDFType(prefix, name, qty) {
+
+        this.prefix = prefix;
+        this.name = name;
+        this.qty = qty;
+    }
+
+    RDFType.prototype.compare = function(rdfType) {
+        return (rdfType.name === this.name && rdfType.prefix === this.prefix);
+    };
+
+    function exist(lst, rdfType) {
         for (var i = 0; i < lst.length; i++) {
             var obj = lst[i];
 
-            if (obj.name === name && obj.prefix === prefix) {
+            if (obj.compare(rdfType)) {
                 return true;
             }
         }
         return false;
     }
 
+    function split(prefixes, URI) {
+        var couple = URI.split("#");
+        var prefix = couple[0] + "#";
+        var name = couple[1];
+
+        for (var pfx in prefixes) {
+            if (prefixes.hasOwnProperty(pfx) && prefixes[pfx] === prefix) {
+                return [pfx, name]
+            }
+        }
+
+        return [prefix, name];
+    }
+
     $scope.typesList = [];
 
     $scope.$on('store.update', function(event, store) {
+        var urisMap = new store.rdf.api.UrisMap();
+
         store.graph(function(err, graph){
             var typeNode = store.rdf.createNamedNode(store.rdf.resolve("rdf:type"));
             var classes = graph.match(null, typeNode, null);
 
             var newList = [];
             for (var i = 0; i < classes.length; i++) {
-                var name = classes.triples[i].object.valueOf();
-                var prefix = 'gnode';
+                var couple = split(store.rdf.prefixes, classes.triples[i].object.valueOf());
 
-                if (!exist(newList, prefix, name)) {
-                    // fetch qty
+                // fetch qty
+                var rdfType = new RDFType(couple[0], couple[1], 0);
 
-                    newList.push({
-                        prefix: prefix,
-                        name: name,
-                        qty: 0
-                    });
+                if (!exist(newList, rdfType)) {
+                    newList.push(rdfType);
                 }
             }
 
