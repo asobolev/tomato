7059ce7ff2eb7ee0d7a161478112410e5f6490e7
asobolev <sobolev.andrey@gmail.com>
2015-10-01 17:31:05 +0200

    added pagination to types list

--- app/controllers/types.js
+++ app/controllers/types.js
@@ -1,6 +1,6 @@
 angular.module('controllers')
 
-.controller('TypesPane', ['$scope', 'store', 'query', function($scope, store, query) {
+.controller('TypesPane', ['$scope', '$filter', 'store', 'query', function($scope, $filter, store, query) {
 
     function RDFType(prefix, name, qty, predicates) {
 
@@ -48,8 +48,25 @@ angular.module('controllers')
         return both[0] + ":" + both[1];
     }
 
+    var searchMatch = function (haystack, needle) {
+        if (!needle) {
+            return true;
+        }
+        return haystack.toLowerCase().indexOf(needle.toLowerCase()) !== -1;
+    };
+
     $scope.storeState = {};
-    $scope.typesList = [];
+    $scope.items = [];
+    $scope.sortingOrder = "id";
+    $scope.reverse = false;
+    $scope.filteredItems = [];
+    $scope.groupedItems = [];
+    $scope.itemsPerPage = 7;
+    $scope.pagedItems = [];
+    $scope.currentPage = 0;
+    $scope.queryBox = {
+        searchText: ""
+    };
 
     $scope.$on('store.update', function(event, storeState) {
         //var urisMap = new store.rdf.api.UrisMap();
@@ -97,7 +114,8 @@ angular.module('controllers')
                 return a.name.localeCompare(b.name);
             });
 
-            $scope.typesList = newList;
+            $scope.items = newList;
+            $scope.search();
             $scope.$apply();
         });
     });
@@ -122,70 +140,130 @@ angular.module('controllers')
         var body = select + where + " ORDER BY ?id";
 
         query.update($scope.storeState.prefixes, body);
-
-
-        /* -------------------
-
-
-        var store = $scope.storeState.store;
-
-        var URI = store.rdf.resolve(rdfType.getURI());
-        var type = store.rdf.resolve("rdf:type"); // RDFModel.defaultContext.rdf
-        var q1 = "SELECT DISTINCT ?s " +
-            "{ ?s <" + type + "> <" + URI + "> . }";
-
-        var q2 = "PREFIX sirota: <http://sirotalab.bio.lmu.de/ontology/0.1#> " +
-            "SELECT DISTINCT * " +
-            "{ ?s a sirota:Drive . " + // note a === <" + type + ">
-            " OPTIONAL { ?s sirota:hasType ?t . } " +
-            " OPTIONAL { ?s sirota:fullScrewTurn ?ft . }" +
-            "}";
-
-        var q3 = "PREFIX sirota: <http://sirotalab.bio.lmu.de/ontology/0.1#> " +
-            "PREFIX gnode: <http://g-node.org/ontology/0.1#> " +
-            "SELECT DISTINCT ?p " +
-            "WHERE { " +
-            " ?s a gnode:RecordingSession . " +
-            " ?s ?p ?lit . " +
-            " FILTER isliteral(?lit) . " +
-            "}";
-
-        var st = new rdfstore.Store({name:'test', overwrite:true}, function(err,str) {
-            str.load(
-                'text/n3',
-                '@prefix test: <http://test.com/> .\
-                 test:A test:prop1 1.\
-                 test:B test:prop1 2.\
-                 test:B test:prop2 test:A.\
-                 test:C test:prop2 test:A.',
-                function (err) {
-
-                    str.execute(
-                        'PREFIX test: <http://test.com/> \
-                        SELECT DISTINCT ?id ?p1 ?p2 \
-                        WHERE {\
-                            ?id ?p ?o .\
-                            OPTIONAL { ?id test:prop1 ?p1 . }\
-                            OPTIONAL { ?p2 test:prop2 ?id . }\
-                        } ORDER BY ?id', // GROUP BY is broken
-                        function (err, results) {
-                            if (!err) {
-                                var a = results.length;
-                            }
-                        }
-                    );
-                });
-        });
-
-
-        $scope.storeState.store.execute(q3, function(err, results){
-            if(!err) {
-                var a = results.length;
-                // results[0].s -> { token: "uri", value: "http://g-node/ont…" }
-            }
-        });
-
-         */
     };
 
+    $scope.search = function () {
+        var txt = $scope.queryBox.searchText;
+
+        $scope.filteredItems = $filter('filter')($scope.items, function (rdfType) {
+            return searchMatch(rdfType.prefix, txt) || searchMatch(rdfType.name, txt);
+        });
+
+        $scope.currentPage = 0;
+        $scope.groupToPages();
+    };
+
+    $scope.groupToPages = function () {
+        $scope.pagedItems = [];
+
+        for (var i = 0; i < $scope.filteredItems.length; i++) {
+            if (i % $scope.itemsPerPage === 0) {
+                $scope.pagedItems[Math.floor(i / $scope.itemsPerPage)] = [ $scope.filteredItems[i] ];
+            } else {
+                $scope.pagedItems[Math.floor(i / $scope.itemsPerPage)].push($scope.filteredItems[i]);
+            }
+        }
+    };
+
+    $scope.range = function (start, end) {
+        var ret = [];
+        if (!end) {
+            end = start;
+            start = 0;
+        }
+        for (var i = start; i < end; i++) {
+            ret.push(i);
+        }
+        return ret;
+    };
+
+    $scope.prevPage = function () {
+        if ($scope.currentPage > 0) {
+            $scope.currentPage--;
+        }
+    };
+
+    $scope.nextPage = function () {
+        if ($scope.currentPage < $scope.pagedItems.length - 1) {
+            $scope.currentPage++;
+        }
+    };
+
+    $scope.setPage = function () {
+        $scope.currentPage = this.n;
+    };
+
+    $scope.sort_by = function(newSortingOrder) {
+        if ($scope.sortingOrder == newSortingOrder)
+            $scope.reverse = !$scope.reverse;
+
+        $scope.sortingOrder = newSortingOrder;
+    };
 }]);
+
+
+
+
+
+/* -------------------
+
+
+ var store = $scope.storeState.store;
+
+ var URI = store.rdf.resolve(rdfType.getURI());
+ var type = store.rdf.resolve("rdf:type"); // RDFModel.defaultContext.rdf
+ var q1 = "SELECT DISTINCT ?s " +
+ "{ ?s <" + type + "> <" + URI + "> . }";
+
+ var q2 = "PREFIX sirota: <http://sirotalab.bio.lmu.de/ontology/0.1#> " +
+ "SELECT DISTINCT * " +
+ "{ ?s a sirota:Drive . " + // note a === <" + type + ">
+ " OPTIONAL { ?s sirota:hasType ?t . } " +
+ " OPTIONAL { ?s sirota:fullScrewTurn ?ft . }" +
+ "}";
+
+ var q3 = "PREFIX sirota: <http://sirotalab.bio.lmu.de/ontology/0.1#> " +
+ "PREFIX gnode: <http://g-node.org/ontology/0.1#> " +
+ "SELECT DISTINCT ?p " +
+ "WHERE { " +
+ " ?s a gnode:RecordingSession . " +
+ " ?s ?p ?lit . " +
+ " FILTER isliteral(?lit) . " +
+ "}";
+
+ var st = new rdfstore.Store({name:'test', overwrite:true}, function(err,str) {
+ str.load(
+ 'text/n3',
+ '@prefix test: <http://test.com/> .\
+ test:A test:prop1 1.\
+ test:B test:prop1 2.\
+ test:B test:prop2 test:A.\
+ test:C test:prop2 test:A.',
+ function (err) {
+
+ str.execute(
+ 'PREFIX test: <http://test.com/> \
+ SELECT DISTINCT ?id ?p1 ?p2 \
+ WHERE {\
+ ?id ?p ?o .\
+ OPTIONAL { ?id test:prop1 ?p1 . }\
+ OPTIONAL { ?p2 test:prop2 ?id . }\
+ } ORDER BY ?id', // GROUP BY is broken
+ function (err, results) {
+ if (!err) {
+ var a = results.length;
+ }
+ }
+ );
+ });
+ });
+
+
+ $scope.storeState.store.execute(q3, function(err, results){
+ if(!err) {
+ var a = results.length;
+ // results[0].s -> { token: "uri", value: "http://g-node/ont…" }
+ }
+ });
+
+ */
